65ffccb910e849a720f14cfbc0e06388c191f194
sheihesinusslon <sheihe24gn@gmail.com>
2024-12-06 17:04:21 +0100

    debug cache key

--- src/api.py
+++ src/api.py
@@ -10,7 +10,7 @@ blockchain = Blockchain(chain_type=ChainType.ARRAY_CHAIN)
 
 @app.get("/mine", status_code=status.HTTP_200_OK)
 def mine() -> dict:
-    """Mining new block: find the proof of work, then add the block to the chain."""
+    """Mining new block: find the proof of work, then add the block to the chain"""
     last_block = blockchain.chain.last_block
     last_proof = last_block.proof
 
@@ -31,7 +31,7 @@ def mine() -> dict:
 
 @app.post("/transactions/new", status_code=status.HTTP_201_CREATED)
 def new_transaction(transaction: Transaction) -> dict:
-    """Create a new transaction and add it to the blockchain."""
+    """Create a new transaction and add it to the blockchain"""
     index = blockchain.add_new_transaction(transaction)
 
     return {"message": f"Transaction will be added to Block {index}."}
